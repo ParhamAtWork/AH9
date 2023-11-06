148da8df65af340b3151898dfb08c74427a4073e
Parham Saniei <parham.saniei@fiserv.com>
2023-11-06 14:16:24 -0500

    fixed test case

--- Source.java
+++ Source.java
@@ -21,18 +21,34 @@ class Source {
         return prev;
     }
     
-    public static void main(String[] args) {
-        Node<String> x = new Node<>("x");
-        Node<String> y = new Node<>("y");
-    
-        x.next = y; // x -> y
-    
-        Node<String> head = reverseList(x); // y -> x
 
-        // Printing solution
+    public static void main(String[] args) {
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+        Node<String> e = new Node<>("e");
+        Node<String> f = new Node<>("f");
+    
+        System.out.println("Hi Dhruv! ");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f; 
+    
+        Node<String> head = reverseList(a); 
+
+        
         while (head != null) {
             System.out.println(head.val);
-            head = head.next;            
+            head = head.next;
+
+
+reverseList(a); // f -> e -> d -> c -> b -> a
         }
     }
 }
+
+ 
